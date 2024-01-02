1e76e6d5653568131067a3414bfc542ad061cee1
Craig Wendorf <cwendorf@uwsp.edu>
2024-01-02 12:27:21 -0600

    Added hypothesis tests

--- R/xCorrelations.R
+++ R/xCorrelations.R
@@ -1,7 +1,7 @@
 # Estimation Approach to Statistical Inference
 ## Correlations
 
-### Corrrelations
+### Estimate
 
 estimateCorrelations <- function(x, ...) {
   UseMethod("estimateCorrelations")
@@ -33,7 +33,7 @@ estimateCorrelations.wsm <- function(moments, corrs, conf.level = .95, ...) {
   }
   results <- as.matrix(results)
   comment(results) <- "Confidence Intervals for the Correlations"
-  class(results) <- c("easi", "cor")
+  class(results) <- c("easi", "intervalsMain")
   return(results)
 }
 
@@ -41,4 +41,59 @@ estimateCorrelations.data.frame <- function(frame, conf.level = .95, ...) {
   moments <- describeMoments(frame)
   corrs <- describeCorrelations(frame)
   estimateCorrelations(moments, corrs, conf.level = conf.level)
+}
+
+### Plot
+
+plotCorrelations <- function(x, ...) {
+  UseMethod("plotCorrelations")
+}
+
+plotCorrelations.data.frame <- function(frame, main = NULL, digits = 3, ylab = "Correlation", xlab = "", mu = 0, line = NULL, rope = NULL, conf.level = .95, values = TRUE, pos = 2, ylim = c(-1.1, 1.1), add = FALSE, pch = 16, connect = FALSE, col = "black", offset = 0, intervals = TRUE) {
+  results <- estimateCorrelations(frame, main = main, digits = digits)
+  plot(results, add = add, main = main, xlab = xlab, ylab = ylab, ylim = ylim, values = values, line = line, rope = rope, digits = digits, connect = connect, pos = pos, col = col, offset = offset, intervals = intervals)
+}
+
+plotCorrelations.wsm <- function(moments, corrs, main = NULL, digits = 3, ylab = "Correlation", xlab = "", mu = 0, line = NULL, rope = NULL, conf.level = .95, values = TRUE, pos = 2, ylim = c(-1.1, 1.1), add = FALSE, pch = 16, connect = FALSE, col = "black", offset = 0, intervals = TRUE) {
+  results <- estimateCorrelations(moments, corrs, main = main, digits = digits)
+  plot(results, add = add, main = main, xlab = xlab, ylab = ylab, ylim = ylim, values = values, line = line, rope = rope, digits = digits, connect = connect, pos = pos, col = col, offset = offset, intervals = intervals)
+}
+
+### Test
+
+testCorrelations <- function(x, ...) {
+  UseMethod("testCorrelations")
+}
+
+testCorrelations.wsm <- function(moments, corrs, ...) {
+  N <- moments[, "N"]
+  rn <- rownames(moments)
+  nr <- nrow(moments)
+  ncomp <- (nr) * (nr - 1) / 2
+  results <- data.frame(matrix(ncol = 5, nrow = ncomp))
+  colnames(results) <- c("R", "SE", "df", "t", "p")
+  comp <- 1
+  for (i in 1:(nr - 1)) {
+    for (j in (i + 1):nr) {
+      rownames(results)[comp] <- paste(rn[i], "&", rn[j])
+      n <- min(N[rn[j]], N[rn[i]])
+      R <- corrs[rn[i], rn[j]]
+      df <- n - 2
+      SE <- sqrt((1 - R^2) / (df))
+      t <- R / SE
+      p <- 2 * (1 - pt(abs(t), df))
+      results[comp, ] <- c(R, SE, df, t, p)
+      comp <- comp + 1
+    }
+  }
+  results <- as.matrix(results)
+  comment(results) <- "Hypothesis Tests for the Correlations"
+  class(results) <- c("easi")
+  return(results)
+}
+
+testCorrelations.data.frame <- function(frame, ...) {
+  moments <- describeMoments(frame)
+  corrs <- describeCorrelations(frame)
+  testCorrelations(moments, corrs)
 }
